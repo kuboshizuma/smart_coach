8e26caad640636a8d22a99095003227e9708abc8
kubo shizuma <forothers.295@gmail.com>
2015-12-13 15:40:10 +0900

    Change routes name class to room

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resources :coaches, only: [:index, :show]
 
   namespace :students do
-    resources :classes do
+    resources :rooms do
       resources :chat_messages, only: :index
     end
   end
