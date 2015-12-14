f37e2231e90bcd97e74be65e38c96c4b88dd60c6
kubo shizuma <forothers.295@gmail.com>
2015-12-14 17:02:16 +0900

    Create coaches/lessons#show

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,7 @@
 class Lesson < ActiveRecord::Base
   belongs_to :genre
-  belongs_to :user
+  belongs_to :coach, class_name: "User"
+  has_many :rooms
 
   mount_uploader :thumbnail, LessonThumbnailUploader
 end
