4739b9de892bd006f2e29215cc5711a8fbbd10ef
kubo shizuma <forothers.295@gmail.com>
2015-12-24 00:10:52 +0900

    Fix color according to genre-color and fix some

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223094452) do
+ActiveRecord::Schema.define(version: 20151223144954) do
 
   create_table "chat_messages", force: :cascade do |t|
     t.text     "message",      limit: 65535
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20151223094452) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "color",      limit: 255
   end
 
   create_table "lesson_shifts", force: :cascade do |t|
