876f7ea39ffb0e08118b56f37265edc11a8ffbe2
kubo shizuma <forothers.295@gmail.com>
2015-12-23 18:27:56 +0900

    Update flash message style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def chat_time(created_at)
     created_at.strftime('%m/%d %H:%M')
   end
+
+  def flash_class_for(flash_type)
+    case flash_type
+    when 'success' then 'alert-success'
+    when 'error'   then 'alert-danger'
+    when 'alert'   then 'alert-warning'
+    when 'notice'  then 'alert-info'
+    end
+  end
 end
