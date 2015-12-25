137a4681a5381547165b18f43886b10a95c7685e
kubo shizuma <forothers.295@gmail.com>
2015-12-25 11:40:31 +0900

    Update lesson edit

--- app/controllers/coaches/lessons_controller.rb
+++ app/controllers/coaches/lessons_controller.rb
@@ -34,7 +34,7 @@ class Coaches::LessonsController < Coaches::CoachesController
 
   def update
     @lesson = Lesson.find(params[:id])
-    @lesson.update(lesson_params)
+    @lesson.update(update_lesson_params)
     redirect_to coaches_lessons_path
   end
 
@@ -52,4 +52,13 @@ class Coaches::LessonsController < Coaches::CoachesController
     end
     params.require(:lesson).permit(:title, :genre_id, :thumbnail, :start_day, :month, :desc_goal, :desc_who, :desc_what, :desc_other, lesson_shifts_attributes: [:weekday, :start_time, :finish_time]).merge(shift_minutes: shift_minutes, coach_id: current_user.id)
   end
+
+  def update_lesson_params
+    shift_minutes = 0
+    params[:lesson][:lesson_shifts_attributes].each_value do |shift|
+      shift_minutes += ((Time.parse(shift[:finish_time]) - Time.parse(shift[:start_time])).to_i / 60).to_i
+    end
+    params.require(:lesson).permit(:title, :genre_id, :thumbnail, :start_day, :month, :desc_goal, :desc_who, :desc_what, :desc_other, lesson_shifts_attributes: [:id, :weekday, :start_time, :finish_time]).merge(shift_minutes: shift_minutes, coach_id: current_user.id)
+  end
+
 end
